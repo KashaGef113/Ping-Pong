4418db2e6e349fa324bd944a7127cdc667c64f49
KashaGef113 <98354683+KashaGef113@users.noreply.github.com>
2023-05-12 18:05:24 +0300

    pong.py
    V2

--- pong.py
+++ pong.py
@@ -1,3 +1,4 @@
+
 from pygame import *
 win_width = 700
 win_height = 500
@@ -35,18 +36,49 @@ class Player(GameSprite):
 rocket1 = Player('player1.png', 0, 390, 30, 100, 10)
 rocket2 = Player('player1.png', 650, 390, 30, 100, 10)
 pong_ball = GameSprite('pong_ball1.png', 300, 300, 50, 50, 9)
+speed_x = 3
+speed_y = 3
+finish = False
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render(
+    'PLAYER 1 LOST!', True, (180, 0, 0)
+)
+font2 = font.Font(None, 35)
+lose2 = font2.render(
+    'PLAYER 2 LOST!', True, (180, 0, 0)
+)
+
 
 while game:
-    window.blit(background,(0, 0))
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-    rocket1.updater()
-    rocket1.reset()
-    rocket2.updatel()
-    rocket2.reset()
-    pong_ball.reset()
+    if finish != True:
+        window.blit(background,(0, 0))
+
+        rocket1.updater()
+        rocket1.reset()
+        rocket2.updatel()
+        rocket2.reset()
+        pong_ball.reset()
+
+        pong_ball.rect.x += speed_x
+        pong_ball.rect.y += speed_y
+
+        if pong_ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+        if pong_ball.rect.x > 700:
+            finish = True
+            window.blit(lose2, (200, 200))
+
+        if pong_ball.rect.y > 450 or pong_ball.rect.y <0:
+            speed_y *= -1
+
+        if sprite.collide_rect(pong_ball, rocket1) or sprite.collide_rect(pong_ball, rocket2):
+                speed_x *= -1
+
     display.update()
     clock.tick(60)
-    #hey
